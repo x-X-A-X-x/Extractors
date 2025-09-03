1085136c3a749ba3ae3659878d2f212aaaf92e98
TechPriest28 <maximilianbenedictsypausal@yahoo.com>
2025-09-03 20:45:08 +0800

    Removed Dupes

--- automation.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "Directory of git folder"
-
-REM 1. Run PowerShell script to extract logs
-powershell -ExecutionPolicy Bypass -File "Extract.ps1"
-
-REM 2. Launch Streamlit app
-streamlit run "Visualizer.py"
-pause
